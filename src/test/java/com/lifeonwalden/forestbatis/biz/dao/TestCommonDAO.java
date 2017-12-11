75ee8ff6264996f7b9514e152b1e8d8a8b66e326
cmanlh <cmanlh@163.com>
2017-12-11 20:15:36 +0800

    correct test source

--- src/test/java/com/lifeonwalden/forestbatis/biz/dao/TestCommonDAO.java
+++ src/test/java/com/lifeonwalden/forestbatis/biz/dao/TestCommonDAO.java
@@ -2,5 +2,5 @@ package com.lifeonwalden.forestbatis.biz.dao;
 
 import com.lifeonwalden.forestbatis.biz.bean.TestMapBean;
 
-public interface TestCommonDAO extends CommonDAO<TestMapBean, TestMapBean> {
+public interface TestCommonDAO extends CommonDAO<TestMapBean> {
 }
--- src/test/java/com/lifeonwalden/forestbatis/biz/dao/TestKeyBasedDAO.java
+++ src/test/java/com/lifeonwalden/forestbatis/biz/dao/TestKeyBasedDAO.java
@@ -2,5 +2,5 @@ package com.lifeonwalden.forestbatis.biz.dao;
 
 import com.lifeonwalden.forestbatis.biz.bean.TestMapBean;
 
-public interface TestKeyBasedDAO extends KeyBasedDAO<TestMapBean, TestMapBean> {
+public interface TestKeyBasedDAO extends KeyBasedDAO<TestMapBean> {
 }
