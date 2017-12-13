74633a3529acc6ab5e8c5f7c530b78c00cd8488c
LuHong <cmanlh@163.com>
2017-12-13 10:52:07 +0800

    add abstract parameter map bean class

---
+++ src/main/java/com/lifeonwalden/forestbatis/biz/bean/AbstractParamMapBean.java
@@ -0,0 +1,39 @@
+package com.lifeonwalden.forestbatis.biz.bean;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AbstractParamMapBean extends AbstractMapBean {
+    protected static Map<String, Class<?>> typeMap = new HashMap<String, Class<?>>();
+
+    @Override
+    public Object put(String key, Object value) {
+        if (null == value) {
+            return dataMap.remove(key);
+        }
+        Class<?> clazz = typeMap.get(key);
+        Object _value;
+        if (null == clazz || clazz.isInstance(value)) {
+            _value = value;
+        } else if (String.class.isInstance(value)) {
+            if (Integer.class.equals(clazz)) {
+                _value = Integer.valueOf((String) value);
+            } else if (BigDecimal.class.equals(clazz)) {
+                _value = new BigDecimal((String) value);
+            } else if (Boolean.class.equals(clazz)) {
+                _value = new Boolean((String) value);
+            } else if (Date.class.equals(clazz)) {
+                _value = new Date(Long.parseLong((String) value));
+            } else if (Long.class.equals(clazz)) {
+                _value = Long.parseLong((String) value);
+            } else {
+                throw new RuntimeException("Invalid data format : " + key);
+            }
+        } else {
+            throw new RuntimeException("Invalid data format : " + key);
+        }
+        return dataMap.put(key, _value);
+    }
+}
