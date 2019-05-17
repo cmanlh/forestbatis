f100cbdedcacdba60e256664b2ece7aad9223277
LuHong <cmanlh@163.com>
2019-05-17 18:57:57 +0800

    mapper implementation

--- forestbatis-core/forestbatis-core-sql/src/test/java/com/lifeonwalden/forestbatis/example/builder/UserBuilder.java
+++ forestbatis-core/forestbatis-core-sql/src/test/java/com/lifeonwalden/forestbatis/example/builder/UserBuilder.java
@@ -6,7 +6,9 @@ import com.lifeonwalden.forestbatis.builder.SelectBuilder;
 import com.lifeonwalden.forestbatis.builder.UpdateBuilder;
 import com.lifeonwalden.forestbatis.example.bean.User;
 import com.lifeonwalden.forestbatis.example.meta.UserTableInfo;
+import com.lifeonwalden.forestbatis.meta.CompoundQuery;
 import com.lifeonwalden.forestbatis.meta.Eq;
+import com.lifeonwalden.forestbatis.meta.Lt;
 import com.lifeonwalden.forestbatis.meta.TableNode;
 
 import java.util.Arrays;
@@ -23,7 +25,7 @@ public class UserBuilder {
 
     public final static DeleteBuilder DELETE = new DeleteBuilder<User>(
             new TableNode(UserTableInfo.TABLE),
-            new Eq(UserTableInfo.Id)
+            new Eq(UserTableInfo.Id).and(new CompoundQuery(new Eq(UserTableInfo.Name).or(new Eq(UserTableInfo.Age))))
     );
 
     public final static UpdateBuilder UPDATE = new UpdateBuilder<User>(
