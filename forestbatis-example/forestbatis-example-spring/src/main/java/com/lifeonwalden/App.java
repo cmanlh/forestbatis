2641dd6ee729509177a8b4d536471dfc97ed1bce
LuHong <cmanlh@163.com>
2019-06-10 17:44:23 +0800

    add forest-spring module

--- forestbatis-example/forestbatis-example-spring/src/main/java/com/lifeonwalden/App.java
+++ forestbatis-example/forestbatis-example-spring/src/main/java/com/lifeonwalden/App.java
@@ -1,17 +1,8 @@
 package com.lifeonwalden;
 
-import com.lifeonwalden.forestbatis.result.RecordHandler;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-    }
-
-    public static void test(RecordHandler recordHandler) {
-
-    }
 }
