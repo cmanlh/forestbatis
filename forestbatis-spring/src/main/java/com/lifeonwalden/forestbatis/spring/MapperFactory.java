9da910cbc483f7e4248772ff6271bc1b14324ef8
卢红 <luhong@orientsec.com.cn>
2019-07-15 11:40:15 +0800

    add datasource

--- forestbatis-spring/src/main/java/com/lifeonwalden/forestbatis/spring/MapperFactory.java
+++ forestbatis-spring/src/main/java/com/lifeonwalden/forestbatis/spring/MapperFactory.java
@@ -31,12 +31,16 @@ public class MapperFactory implements InitializingBean {
         }
     }
 
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public DataSource getDataSource() {
-        return dataSource;
+    public Config getConfig() {
+        return config;
     }
 
     public void setConfig(Config config) {
