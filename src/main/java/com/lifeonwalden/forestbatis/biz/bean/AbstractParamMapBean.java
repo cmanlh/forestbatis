1f69cce89b8abb727c5cc4b182494f23025691b2
卢红 <luhong@orientsec.com.cn>
2019-07-19 13:33:53 +0800

    fix list

--- src/main/java/com/lifeonwalden/forestbatis/biz/bean/AbstractParamMapBean.java
+++ src/main/java/com/lifeonwalden/forestbatis/biz/bean/AbstractParamMapBean.java
@@ -1,7 +1,10 @@
 package com.lifeonwalden.forestbatis.biz.bean;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class AbstractParamMapBean extends AbstractMapBean {
     private static final long serialVersionUID = 2825637947183339992L;
@@ -17,10 +20,12 @@ public class AbstractParamMapBean extends AbstractMapBean {
         }
         Class<?> clazz = _getType(key);
         Object _value = null;
+        Class<?> valueClazz = value.getClass();
         if (null == clazz || clazz.isInstance(value)) {
             _value = value;
         } else if (String.class.isInstance(value)) {
             if (Integer.class.equals(clazz)) {
+                _value = Integer.parseInt((String) value);
             } else if (BigDecimal.class.equals(clazz)) {
                 _value = new BigDecimal((String) value);
             } else if (Boolean.class.equals(clazz)) {
@@ -32,9 +37,60 @@ public class AbstractParamMapBean extends AbstractMapBean {
             } else {
                 throw new RuntimeException("Invalid data format : " + key);
             }
+        } else if (valueClazz.equals(Long.class)) {
+            if (Date.class.equals(clazz)) {
+                _value = new Date((long) value);
+            } else if (BigDecimal.class.equals(clazz)) {
+                _value = BigDecimal.valueOf((long) value);
+            } else {
+                _value = value;
+            }
+        } else if (valueClazz.equals(Double.class)) {
+            if (BigDecimal.class.equals(clazz)) {
+                _value = BigDecimal.valueOf((double) value);
+            } else {
+                _value = value;
+            }
+        } else if (valueClazz.equals(Float.class)) {
+            if (BigDecimal.class.equals(clazz)) {
+                _value = BigDecimal.valueOf((float) value);
+            } else {
+                _value = value;
+            }
+        } else if (valueClazz.equals(Integer.class)) {
+            if (BigDecimal.class.equals(clazz)) {
+                _value = BigDecimal.valueOf((int) value);
+            } else {
+                _value = value;
+            }
+        } else if (List.class.isInstance(value)) {
+            if (AbstractParamMapBean.class.equals(clazz.getSuperclass())) {
+                List _list = (List) value;
+                List list = new ArrayList();
+                _list.forEach(item -> list.add(_move((Map<String, Object>) item, clazz)));
+                _value = list;
+            }
         } else {
             throw new RuntimeException("Invalid data format : " + key);
         }
         return dataMap.put(key, _value);
+    }
+
+    private Object _move(Map<String, Object> value, Class<?> clazz) {
+        Map<String, Object> target;
+        try {
+            target = (Map<String, Object>) clazz.newInstance();
+            for (Entry<String, Object> entry : value.entrySet()) {
+                target.put(entry.getKey(), entry.getValue());
+            }
+        } catch (InstantiationException e) {
+
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+
+            throw new RuntimeException(e);
+        }
+
+        return target;
     }
 }
